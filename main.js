81942aa7c11b0316aceb445b1dffa12a69a36d6e
NushiSid <puja.ramani@gmail.com>
2025-10-26 10:24:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var Movie=["https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200",
+    "https://m.media-amazon.com/images/M/MV5BMWI1OGM4YjQtNmIxNi00YmE2LWJkNTAtY2Q0YjU4NTI5NWQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_FMjpg_UX1000_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BNTU1MzgyMDMtMzBlZS00YzczLThmYWEtMjU3YmFlOWEyMjE1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"]
+var Number=0
+function Movies(){
+    Number=Number+1
+    if(Number>3){
+        Number=0
+    }
+    document.getElementById("Avatar").src=Movie[Number]
+
+
+}
